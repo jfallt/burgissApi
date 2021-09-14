b5ccb108f427eccf7c0ba4ffc366187fd723514f
jfallt <fallt.jared@gmail.com>
2021-09-14 19:14:23 -0400

    increment version number with changes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 DESCRIPTION = 'An api request package for Burgiss'
 LONG_DESCRIPTION = 'A package that makes it easy to make requests to the Burgiss API by simplifying the JWT token auth and various endpoints'
 
