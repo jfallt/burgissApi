306330b8088297870c24cf09de0c24c6621c589c
jfallt <fallt.jared@gmail.com>
2021-10-20 15:52:46 -0400

    move test class here, set up for cmd line calls

---
+++ tests/conftest.py
@@ -0,0 +1,84 @@
+import configparser
+import datetime
+from datetime import datetime, timedelta
+import pytest
+from burgissApiWrapper.burgissApi import tokenAuth, init, session, transformResponse
+
+class testApiResponses():
+    def __init__(self, clientId=None, username=None, password=None, urlToken=None, urlApi=None, analyticsUrlApi=None, assertionType=None, scope=None) -> None:
+        self.tokenInit = tokenAuth(clientId, username, password, urlToken, urlApi, analyticsUrlApi, assertionType, scope)
+        self.initSession = init(clientId, username, password, urlToken, urlApi, analyticsUrlApi, assertionType, scope)
+        self.burgissSession = session(clientId, username, password, urlToken, urlApi, analyticsUrlApi, assertionType, scope)
+        self.transformResponse = transformResponse()
+
+        self.endpoints = ['orgs', 'investments', 'portfolios', 'assets', 'LookupData']
+
+    def testGetBurgissApiToken(self, secret=None):
+        token = self.tokenInit.getBurgissApiToken(secret)
+        assert len(token) != 0
+
+    def testTokenReset(self):
+        tokenExpiration = self.initSession.tokenExpiration
+        self.initSession.tokenExpiration = datetime.now() + timedelta(seconds=3600)
+        self.initSession.checkTokenExpiration()
+        assert tokenExpiration != self.initSession.tokenExpiration
+
+    def testProfileRequest(self):
+        profileResponse = self.initSession.requestWrapper('profiles')
+        assert profileResponse.status_code == 200
+
+    def testRequestResponseCode(self, endpoint):
+        response = self.burgissSession.request(endpoint)
+        assert response.status_code == 200
+
+    def testOptionalParametersRequestResponseCode(self, endpoint, optionalParameters):
+        response = self.burgissSession.request(
+            endpoint, optionalParameters=optionalParameters)
+        assert response.status_code == 200
+
+    def testProfileIdAsHeaderResponse(self, endpoint):
+        response = self.burgissSession.request(endpoint, profileIdAsHeader=True)
+        assert response.status_code == 200
+
+    def testDataTransformation(self, endpoint):
+        response = self.transformResponse.getData(endpoint)
+        assert isinstance(response, pd.DataFrame) == True
+        assert len(response) > 0
+
+def pytest_addoption(parser):
+    config = configparser.ConfigParser()
+    config.read_file(open('config.cfg'))
+    parser.addoption("--user", action="store", default=config.get('API', 'user'))
+    parser.addoption("--pw", action="store", default=config.get('API', 'pw'))
+    parser.addoption("--clientId", action="store", default=config.get('API', 'clientId'))
+    parser.addoption("--tokenUrl", action="store", default=config.get('API', 'tokenUrl'))
+    parser.addoption("--apiUrl", action="store", default=config.get('API', 'apiUrl'))
+    parser.addoption("--apiUrlAnalytics", action="store", default=config.get('API', 'apiUrlAnalytics'))
+    parser.addoption("--assertionType", action="store", default=config.get('API', 'assertionType'))
+    parser.addoption("--scope", action="store", default=config.get('API', 'scope'))
+    parser.addoption("--profileIdType", action="store", default=config.get('API', 'profileIdType'))
+    parser.addoption("--secret", action="store")
+
+@pytest.fixture(scope='session')
+def testApiResponsesFixture(pytestconfig):
+    """
+    
+    """
+    clientId= pytestconfig.getoption("clientId")
+    user = pytestconfig.getoption("user")
+    pw = pytestconfig.getoption("pw")
+    tokenUrl = pytestconfig.getoption("tokenUrl")
+    apiUrl = pytestconfig.getoption("apiUrl")
+    apiUrlAnalytics = pytestconfig.getoption("apiUrlAnalytics")
+    assertionType = pytestconfig.getoption("assertionType")
+    scope = pytestconfig.getoption("scope")
+    test = testApiResponses(clientId, user, pw, tokenUrl, apiUrl, apiUrlAnalytics, assertionType, scope)
+
+    # Session
+    test.burgissSession.profileIdType = pytestconfig.getoption("profileIdType")
+
+    return test
+
+@pytest.fixture(scope='session')
+def secret(pytestconfig):
+    return pytestconfig.getoption("secret")
