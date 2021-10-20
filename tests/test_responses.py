373b9f07437127515a1ed59e28259484b309f1c7
jfallt <fallt.jared@gmail.com>
2021-10-20 11:34:42 -0400

    test error handling

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,16 +1,30 @@
 import pytest
-from burgissApiWrapper.burgissApi import testApiResponses
+from burgissApiWrapper.burgissApi import (ApiConnectionError,
+                                          responseCodeHandling,
+                                          testApiResponses, tokenErrorHandling)
+from requests.models import Response
 
 testApiResponses = testApiResponses()
+
+
 def testTokenGen():
     testApiResponses.testGetBurgissApiToken()
+
+def testTokenExpiration():
+    testApiResponses.testTokenReset()
+
 def testGetProfile():
     testApiResponses.testProfileRequest()
+
+
 def testOptionalParameters():
     testApiResponses.testOptionalParametersRequestResponseCode('investments',
-    '&includeInvestmentNotes=false&includeCommitmentHistory=false&includeInvestmentLiquidationNotes=false')
+                                                               '&includeInvestmentNotes=false&includeCommitmentHistory=false&includeInvestmentLiquidationNotes=false')
+
+
 def testProfileIdAsHeader():
     testApiResponses.testProfileIdAsHeaderResponse('LookupValues')
+
 
 @pytest.mark.parametrize('endpoint', testApiResponses.endpoints)
 def testEndpoints(endpoint):
@@ -19,7 +33,34 @@ def testEndpoints(endpoint):
     """
     testApiResponses.testRequestResponseCode(endpoint)
 
+
 @pytest.mark.parametrize('endpoint', testApiResponses.endpoints)
 def testDataTransformation(endpoint):
     "Test if endpoint returns a flattened dataframe with length > 0"
     testApiResponses.testDataTransformation(endpoint)
+
+# Test token response handling
+validTokenExample = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+
+exampleTokenResponse = [
+    {'error': 'invalid_scope'},
+    {'status_code': 500},
+    {'some_other_key': 'data'}
+]
+
+def testTokenResponse():
+    assert tokenErrorHandling({'access_token': validTokenExample}) == validTokenExample
+
+@pytest.mark.parametrize('tokenResponseJson', exampleTokenResponse)
+def testTokenExceptions(tokenResponseJson):
+    with pytest.raises(ApiConnectionError):
+        tokenErrorHandling(tokenResponseJson)
+
+responseCodes = [400, 401, 404, 500, 503]
+
+@pytest.mark.parametrize('responseCode', responseCodes)
+def testResponseErrorHandling(responseCode):
+    response = Response()
+    response.status_code = responseCode
+    with pytest.raises(ApiConnectionError):
+        responseCodeHandling(response)
